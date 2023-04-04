dd11733a41c8a07c3d0bfcf0e45c8184bdc57cf8
unknown <thanhnhan54>
2023-04-04 21:35:32 +0700

    casestudy1

--- case.js
+++ case.js
@@ -1,9 +1,9 @@
 
 
 class product {
-    constructor(image, discount, name, price, info) {
+    constructor(image,  name, price, info) {
         this.image = image;
-        this.discount = discount;
+        
         this.name = name;
         this.price = price;
         this.info = info;
@@ -11,83 +11,97 @@ class product {
 }
 let products = [
     new product('https://bizweb.dktcdn.net/100/342/645/products/0c6ae505cd0029d8918b8a6bebc82fe2.jpg?v=1545572483250',
-                'Sale', 'Giày đá bóng sân cỏ nhân tạo', [180000, 150000], 'Thêm vào giỏ hàng'),
+                 'Giày đá bóng sân cỏ nhân tạo', 180000, 'Thêm vào giỏ hàng'),
     new product('https://bizweb.dktcdn.net/thumb/medium/100/342/645/products/men-s-original-canvas-casual-skate-shoes.png?v=1545564063607',
-                'Sale', 'Giày đá bóng sân cỏ nhân tạo', [200000, 170000], 'Thêm vào giỏ hàng'),
+                 'Giày thể thau nữ', 200000,  'Thêm vào giỏ hàng'),
     new product('https://bizweb.dktcdn.net/thumb/medium/100/342/645/products/1-743624de-c3ad-40a5-a64e-6ac148652219-b0a6d7fb-0c11-4e3e-83a8-44d4e00066b5.png?v=1545572153183',
-                'Sale', 'Giày đá bóng sân cỏ nhân tạo', [1800000, 1500000], 'Thêm vào giỏ hàng'),
+                 'Giày Sneaker giới trẻ',  190000, 'Thêm vào giỏ hàng'),
     new product('https://bizweb.dktcdn.net/thumb/medium/100/342/645/products/men-s-increase-shoes-8cm-height-increase.jpg?v=1545563234533',
-                'Sale', 'Giày đá bóng sân cỏ nhân tạo', [1800000, 1500000], 'Thêm vào giỏ hàng'),
+                 'Giày chạy bộ nam nữ',  180000, 'Thêm vào giỏ hàng'),
     new product('https://bizweb.dktcdn.net/thumb/medium/100/342/645/products/women-casual-shoes-new-design-rubber-slide.jpg?v=1545563755483',
-                'Sale', 'Giày đá bóng sân cỏ nhân tạo', [1800000, 1500000], 'Thêm vào giỏ hàng'),
+                 'Giày cổ cao nam', 160000, 'Thêm vào giỏ hàng'),
 
 ]
 function renderProducts() {
     for (let i = 0; i < products.length; i++) {
         document.querySelector('.forth').innerHTML +=
          `
-        <div class = "pro">
-            <div class ="pro1">
+        <div class = "product-item">
+            
                 <img class = "image1" src="${products[i].image}">
-                <p class = " discount" >${products[i].discount}</p>
-               
-            </div>
-            <div class ="pro2">    
-                <p class = "name">${products[i].name}</p>        
-                <span><del>${products[i].price[0]}</del></span> 
-                <span class = "price">${products[i].price[1]}</span> <br>            
+                <div class ="product-item-text">    
+                    <p><span>${products[i].price}</span></p>        
+                    <h3>${products[i].name}</h3>          
+                </div>
                 <button class="btn-cart">${products[i].info}</button>
+                 
+                
             </div>
-        </div>
-        `
-    }
+            `
+        }
+               
+            
     
 }
 renderProducts();
-class product1 {
-    constructor(image, discount, name, price, info) {
-        this.image = image;
-        this.discount = discount;
-        this.name = name;
-        this.price = price;
-        this.info = info;
-    }
-}
-let products1 = [
-    new product1('https://bizweb.dktcdn.net/100/342/645/products/0c6ae505cd0029d8918b8a6bebc82fe2.jpg?v=1545572483250',
-                'Sale', 'Giày đá bóng sân cỏ nhân tạo', [180000, 150000], 'Thêm vào giỏ hàng'),
-    new product1('https://bizweb.dktcdn.net/thumb/medium/100/342/645/products/men-s-original-canvas-casual-skate-shoes.png?v=1545564063607',
-                'Sale', 'Giày đá bóng sân cỏ nhân tạo', [200000, 170000], 'Thêm vào giỏ hàng'),
-    new product1('https://bizweb.dktcdn.net/thumb/medium/100/342/645/products/1-743624de-c3ad-40a5-a64e-6ac148652219-b0a6d7fb-0c11-4e3e-83a8-44d4e00066b5.png?v=1545572153183',
-                'Sale', 'Giày đá bóng sân cỏ nhân tạo', [1800000, 1500000], 'Thêm vào giỏ hàng'),
-    new product1('https://bizweb.dktcdn.net/thumb/medium/100/342/645/products/men-s-increase-shoes-8cm-height-increase.jpg?v=1545563234533',
-                'Sale', 'Giày đá bóng sân cỏ nhân tạo', [1800000, 1500000], 'Thêm vào giỏ hàng'),
-    new product1('https://bizweb.dktcdn.net/thumb/medium/100/342/645/products/women-casual-shoes-new-design-rubber-slide.jpg?v=1545563755483',
-                'Sale', 'Giày đá bóng sân cỏ nhân tạo', [1800000, 1500000], 'Thêm vào giỏ hàng'),
 
-]
-function renderProducts1() {
-    for (let i = 0; i < products1.length; i++) {
-        document.querySelector('.forth1').innerHTML +=
-         `
-        <div class = "pro">
-            <div class ="pro1">
-                <img class = "image1" src="${products1[i].image}">
-                <p class = " discount" >${products1[i].discount}</p>
-               
-            </div>
-            <div class ="pro2">    
-                <p class = "name">${products1[i].name}</p>        
-                <span><del>${products1[i].price[0]}</del></span> 
-                <span class = "price">${products1[i].price[1]}</span> <br>            
-                </div>
-                <button class="btn-cart">${products1[i].info}</button>
-        </div>
-        `
-    }
+
+let btn = document.querySelectorAll('button');
+btn.forEach(function(button, index){
+  button.addEventListener('click', function(event){
+    let btnItem = event.target;
+    let product = btnItem.parentElement
+    let productImg = product.querySelector('img').src
+    let productName = product.querySelector('h3').innerText
+    let productPrice = product.querySelector('span').innerText 
+    addCart(productPrice,productImg, productName)
+   
+  })
+  
+});
+function  addCart(productPrice, productImg, productName){
+    let addtr = document.createElement('tr');
+    let cartItem = document.querySelectorAll('tbody tr')
+    for(i = 0; i < cartItem.length; i++){
+        let productT = document.querySelectorAll('.title')
+        if(productT[i].innerHTML == productName){
+            alert('sản phẩm của bạn đã có trong giỏ hàng')
+            return;
+        }
+     }
+    let trcontent = ' <tr><td style="display: flex; align-items: center;"><img style="width: 70px;" src="'+productImg+'"><span class="title">'+productName+'</span></td><td><p><span  class="prices">'+productPrice+'</span></p></td><td><input style="width: 30px; outline: none;" type="number" value="1" min="1"></td><td style="cursor: pointer;"><span class="del">Xóa</span></td></tr>'
+    addtr.innerHTML = trcontent
+    let cartTable = document.querySelector('tbody')
+    cartTable.append(addtr)
+
+    carttotal()
+    deleteCart()
     
+  }
+
+function carttotal(){
+    let cartItem = document.querySelectorAll('tbody tr')
+    let total = 0;
+    for(i = 0; i < cartItem.length; i++){
+        let inputValue = cartItem[i].querySelector('input').value
+        let productPrice = cartItem[i].querySelector('.prices').innerHTML
+        s = inputValue * productPrice 
+        total += s
+        
+    }
+    let cartS = document.querySelector('.price-total span')
+    cartS.innerHTML = total;
 }
-renderProducts1();
 
 
-
+function deleteCart(){
+    let cartItem = document.querySelectorAll('tbody tr')
+    for(i = 0; i < cartItem.length; i++){
+        let productT = document.querySelectorAll('.del')
+        productT[i].addEventListener('click', function(event){
+            let cartDel = event.target;
+            let cartItemr = cartDel.parentElement.parentElement
+            cartItemr.remove();
+        })
+     }
+}
